9719cccf52f1df7914b5bdbf5b32634b8ae32a95
Timothy Makarov <timothy.makarov@gmail.com>
2020-04-01 12:59:05 +0300

    Added new data. Bug fixed: correct days since. Notebook sources R-file.

--- COVID-19.R
+++ COVID-19.R
@@ -3,6 +3,7 @@
 # https://gist.github.com/timothy-makarov/c5a8d382aaa646fea47d4e0beb354e91
 #
 
+library(dplyr)
 library(ggplot2)
 
 rm(list = ls())
@@ -38,45 +39,53 @@ df <- data.frame(
     1036,  # https://t.me/COVID2019_official/113
     1264,  # https://t.me/COVID2019_official/121
     1534,  # https://t.me/COVID2019_official/128
-    1836   # https://t.me/COVID2019_official/138
+    1836,  # https://t.me/COVID2019_official/138
+    2337,  # https://t.me/COVID2019_official/153
+    2777   # https://t.me/COVID2019_official/160
   )
 )
 
-df$days_since_mar01 <- seq(1, length(df$infected))
-df$inc <- c(0, diff(df$infected))
+df$day0 <- seq(0, length(df$infected) - 1)
+df$date <- as.Date('2020-03-01') + df$day0
+df$growth <- c(1, diff(df$infected))
 df$infected_log <- log2(df$infected)
 
-lm1 <- lm(formula = infected_log ~ days_since_mar01, data = df)
+df <- df %>%
+  filter(infected > 10)
+
+lm1 <- lm(formula = infected_log ~ day0, data = df)
 k <- lm1$coefficients[[2]]
 b <- lm1$coefficients[[1]]
 
-df$no_trend <- df$days_since_mar01 * k + b - df$infected_log
-t.test(df$no_trend)
+df$deviations <- df$day0 * k + b - df$infected_log
+t.test(df$deviations)
 
-frcst_date <- as.Date('2020-03-31')
+frcst_date <- as.Date('2020-04-01')
 dsm01 <- as.numeric(frcst_date - as.Date('2020-03-01')) + 1
 frcst_infected <- floor(2 ^ (k * dsm01 + b))
 frcst_infected
 
-pop <- 1E+4
-pop_log <- log2(pop)
-pop_dsm01 <- (pop_log - b) / k
-pop_dsm01_cal <- as.Date('2020-03-01') + pop_dsm01
-pop_dsm01_cal
+md <- data.frame(
+  day0 = seq(0, 180)
+)
+md$date <- md$day0 + as.Date('2020-03-01')
 
-ggplot(df, aes(days_since_mar01, infected, colour = inc)) +
+md <- md %>%
+  filter(day0 >= first(df$day0))
+
+ggplot(df, aes(date, infected, colour = growth)) +
   geom_line() +
   geom_point() +
   ggtitle('Total number of infected') +
   theme(plot.title = element_text(hjust = 0.5))
 
-ggplot(df, aes(days_since_mar01, infected_log, colour = inc)) +
+ggplot(df, aes(date, infected_log, colour = growth)) +
   geom_point() +
   geom_smooth(method = lm, formula = y ~ x) +
   ggtitle('Total number of infected in a log scale') +
   theme(plot.title = element_text(hjust = 0.5))
 
-ggplot(df, aes(days_since_mar01, no_trend, colour = inc)) +
+ggplot(df, aes(date, deviations, colour = growth)) +
   geom_point() +
   geom_line() +
   geom_smooth(method = lm, formula = y ~ x) +
