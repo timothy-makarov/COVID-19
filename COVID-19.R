6800a1ba6d60ceb5461a9335fac919532ad482fb
Timothy Makarov <timothy.makarov@gmail.com>
2020-04-04 02:06:28 +0300

    Added new data. New feature added: SIR ODE.

--- COVID-19.R
+++ COVID-19.R
@@ -3,6 +3,7 @@
 # https://gist.github.com/timothy-makarov/c5a8d382aaa646fea47d4e0beb354e91
 #
 
+library(deSolve)
 library(dplyr)
 library(ggplot2)
 
@@ -42,7 +43,8 @@ df <- data.frame(
     1836,  # https://t.me/COVID2019_official/138
     2337,  # https://t.me/COVID2019_official/153
     2777,  # https://t.me/COVID2019_official/160
-    3548   # https://t.me/COVID2019_official/168
+    3548,  # https://t.me/COVID2019_official/168
+    4149   # https://t.me/COVID2019_official/179
   )
 )
 
@@ -83,3 +85,47 @@ ggplot(df, aes(date, error, colour = growth)) +
 ggplot(df, aes(date, error_log, colour = growth)) +
   geom_point() +
   geom_smooth(method = loess, formula = y ~ x)
+
+#
+# -=[ ODE ]=-
+# https://kingaa.github.io/thid/odes/ODEs_in_R.pdf
+#
+
+# beta - transmition rate
+# gamma - recovery rate
+
+closed.sir.model <- function(t, x, params) {
+  S <- x[1]
+  I <- x[2]
+  R <- x[3]
+  
+  beta <- params["beta"]
+  gamma <- params["gamma"]
+  
+  dSdt <- -beta * S * I
+  dIdt <- beta * S * I - gamma * I
+  dRdt <- gamma * I
+  
+  dxdt <- c(dSdt, dIdt, dRdt)
+  
+  list(dxdt)
+}
+
+params <- c(beta = 400, gamma = 365 / 13)
+
+times <- seq(from = 0, to = 60 / 365, by = 1 / 365 / 4)
+xstart <- c(S = 0.999, I = 0.001, R = 0.000)
+
+out <- as.data.frame(ode(
+  func = closed.sir.model,
+  y = xstart,
+  times = times,
+  parms = params
+))
+
+
+plot(S ~ time, data = out, type = 'l', col = 'blue')
+lines(I ~ time, data = out, col = 'brown')
+lines(R ~ time, data = out, col = 'green')
+
+paste0('R_0 = ', round(params['beta'] / params['gamma'], 2))
