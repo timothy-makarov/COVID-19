6a457b1145575465f01648d8f5cdfc7b631b2a6a
Timothy Makarov <timothy.makarov@gmail.com>
2020-04-11 16:39:05 +0300

    Added new data. Implemented new report.

--- COVID-19.R
+++ COVID-19.R
@@ -8,6 +8,9 @@ library(ggplot2)
 
 rm(list = ls())
 
+# Environment
+setwd("~/Git/COVID-19")
+
 # Import
 df0 <- read.csv('COVID-19.csv')
 df0$date <- as.Date(df0$date)
@@ -32,48 +35,5 @@ df0$infected_per_recovered <- df0$infected / df0$recovered
 df0$infected_per_deceased <- df0$infected / df0$deceased
 df0$recovered_per_deceased <- df0$recovered / df0$deceased
 
-# Plotting
-ggplot(df0) +
-  geom_point(aes(date, infected), color = 'brown', size = 0.75) +
-  geom_line(
-    aes(date, infected_lm0),
-    color = 'brown',
-    size = 0.25,
-    linetype = 'dashed'
-  )
-
-ggplot(df0) +
-  geom_point(aes(date, infected_log2), size = 0.75) +
-  geom_line(aes(date, infected_l2lm0),
-            size = 0.25,
-            linetype = 'dashed')
-
-ggplot(df0) +
-  geom_point(aes(date, removed_log2), size = 0.75) +
-  geom_line(aes(date, removed_l2lm0),
-            size = 0.25,
-            linetype = 'dashed')
-
-ggplot(df0) +
-  geom_point(aes(date, removed), size = 0.75) +
-  geom_line(aes(date, removed_lm0),
-            size = 0.25,
-            linetype = 'dashed')
-
-ggplot(df0) +
-  geom_point(aes(date, infected_per_recovered), size = 0.75) +
-  geom_line(aes(date, infected_per_recovered),
-            size = 0.25,
-            linetype = 'dashed')
-
-ggplot(df0) +
-  geom_point(aes(date, infected_per_deceased), size = 0.75) +
-  geom_line(aes(date, infected_per_deceased),
-            size = 0.25,
-            linetype = 'dashed')
-
-ggplot(df0) +
-  geom_point(aes(date, recovered_per_deceased), size = 0.75) +
-  geom_line(aes(date, recovered_per_deceased),
-            size = 0.25,
-            linetype = 'dashed')
+# Presentation
+df0_view <- df0 %>% select(date, infected, recovered, deceased)
