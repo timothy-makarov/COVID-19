b6d1d3717f3417ef675cd3ded6ceb1782aa536dc
Timothy Makarov <timothy.makarov@gmail.com>
2020-04-12 16:54:10 +0300

    Added new data. Updated report.

--- COVID-19.R
+++ COVID-19.R
@@ -18,6 +18,8 @@ df0$infected_growth <- c(1, diff(df0$infected))
 df0$infected_log2 <- log2(df0$infected)
 df0$removed <- df0$recovered + df0$deceased
 df0$removed_log2 <- log2(df0$removed)
+df0$recovered_log2 <- log2(df0$recovered)
+df0$deceased_log2 <- log2(df0$deceased)
 
 # Filtering
 df0 <- df0 %>% filter(date > '2020-03-31')
@@ -25,15 +27,35 @@ df0 <- df0 %>% filter(date > '2020-03-31')
 # Analysis
 infected_l2lm0 <- lm(formula = infected_log2 ~ date, df0)
 df0$infected_l2lm0 <- predict(infected_l2lm0, df0)
-df0$infected_lm0 <- 2 ^ df0$infected_l2lm0
+df0$infected_lm0 <- round(2 ^ df0$infected_l2lm0)
 
 removed_l2lm0 <- lm(formula = removed_log2 ~ date, df0)
 df0$removed_l2lm0 <- predict(removed_l2lm0, df0)
-df0$removed_lm0 <- 2 ^ df0$removed_l2lm0
+df0$removed_lm0 <- round(2 ^ df0$removed_l2lm0)
+
+recovered_l2lm0 <- lm(formula = recovered_log2 ~ date, df0)
+df0$recovered_l2lm0 <- predict(recovered_l2lm0, df0)
+df0$recovered_lm0 <- round(2 ^ df0$recovered_l2lm0)
+
+deceased_l2lm0 <- lm(formula = deceased_log2 ~ date, df0)
+df0$deceased_l2lm0 <- predict(deceased_l2lm0, df0)
+df0$deceased_lm0 <- round(2 ^ df0$deceased_l2lm0)
 
 df0$infected_per_recovered <- df0$infected / df0$recovered
 df0$infected_per_deceased <- df0$infected / df0$deceased
 df0$recovered_per_deceased <- df0$recovered / df0$deceased
 
 # Presentation
-df0_view <- df0 %>% select(date, infected, recovered, deceased)
+df0_data <- df0 %>%
+  filter(!is.na(infected)) %>%
+  select(date,
+         infected,
+         recovered,
+         deceased)
+
+df0_forecast <- df0 %>%
+  filter(date >= Sys.Date()) %>%
+  select(date,
+         infected_lm0,
+         recovered_lm0,
+         deceased_lm0)
